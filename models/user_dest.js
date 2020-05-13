14269b31c9fe428decf33e72dd5733bfe9aecf2c
kcenters <kristincenters@gmail.com>
2020-05-12 21:30:01 -0500

    added indexes to user_dest model

--- models/user_dest.js
+++ models/user_dest.js
@@ -1,24 +1,37 @@
-module.exports = function(sequelize, DataTypes) {
-    var userDest = sequelize.define("user_dests", {
-      lat: {
-        type: DataTypes.DECIMAL,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      lng: {
-        type: DataTypes.DECIMAL,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      dest: {
-        type: DataTypes.STRING,
-        allowNull: false,
+module.exports = function (sequelize, DataTypes) {
+  var userDest = sequelize.define("user_dests", {
+    destination_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
       }
-    });
-    return userDest;
-  };
-  
+    },
+    destination_lat: {
+      type: DataTypes.DECIMAL(9, 6),
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    destination_lon: {
+      type: DataTypes.DECIMAL(9, 6),
+      allowNull: false,
+    },
+    indexes: [
+      {
+        unique: false,
+        fields: ["destination_id", "destination_lat", "destination_lon"]
+      },
+      {
+        unique: true,
+        fields: ['user_id']
+      }
+    ]
+  });
+  userDest.associate = models => {
+    userDest.belongsTo(models.user);
+    userDest.hasMany(models.destination);
+  }
+  return userDest;
+};
