51ce7030a7ba61a77fa09f6b62ef18b550e7e253
kcenters <kristincenters@gmail.com>
2020-05-16 01:34:55 -0500

    added jawsdb to config file

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
   production: {
     username: process.env.DB_PROD_USERNAME,
     password: process.env.DB_PROD_PASSWORD,
-    database: process.env.DB_PROD_DATABASE,
+    "use_env_variable": "JAWSDB_URL",
     host: process.env.DB_PROD_HOST,
     dialect: 'mysql'
   }
