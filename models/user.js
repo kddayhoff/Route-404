1a4a5c5fd3de02aeca46c1a0141c321c5e17793f
kcenters <kristincenters@gmail.com>
2020-05-16 10:23:26 -0500

    added passport npm pkges, authenticated file, passport.js

--- models/user.js
+++ models/user.js
@@ -1,19 +1,31 @@
-module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    destinations: DataTypes.STRING,
+// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
+var bcrypt = require("bcryptjs");
+// Creating our User model
+module.exports = function (sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    // The email cannot be null, and must be a proper email before creation
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
-
-
-//   User.associate = function(models) {
-//     User.hasMany(models.destNotes, {
-//       onDelete: "cascade"
-// //handling deletions of notes associated with users
-
-//     })
-    //associating notes with user destinations from destination.js
-
-  
-
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  User.addHook("beforeCreate", function (user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
   return User;
 };
