75904f2a4ec6e46ec37b1d6fe10864bf9ae5b1d9
kcenters <kristincenters@gmail.com>
2020-05-18 14:06:10 -0500

    user signup working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -19,31 +19,56 @@ module.exports = function (app) {
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+
+  app.get("/signup", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/userdest", function (req, res) {
+    console.log('here')
+    res.sendFile(path.join(__dirname, "../public/userdest.html"));
+  });
+
+
   //this will load user specific profile along with journal/destination activities
   app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/userdest.html"));
   });
 
 
-app.get("/", function (req, res) {
+  //app.get("/", function (req, res) {
   // If the user already has an account send them to the members page
-  if (req.user) {
-    res.redirect("/userdest.html");
-  }
-  res.sendFile(path.join(__dirname, "../public/signup.html"));
-});
+  //if (req.user) {
+  // res.redirect("/userdest.html");
+  //}
+  //res.sendFile(path.join(__dirname, "../public/signup.html"));
+  //});
 
-app.get("/login", function (req, res) {
-  // If the user already has an account send them to the members page
-  if (req.user) {
-    res.redirect("/userdest");
-  }
-  res.sendFile(path.join(__dirname, "../public/login.html"));
-});
 
-// Here we've add our isAuthenticated middleware to this route.
-// If a user who is not logged in tries to access this route they will be redirected to the signup page
-app.get("/userdest", isAuthenticated, function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/userdest.html"));
-});
+
+  app.get("/login", function (req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/userdest");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+
+}
+
+
+
+function isLoggedIn(req, res, next) {
+
+
+  // If the user is logged in, continue with the request to the restricted route
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  // If the user isn't logged in, redirect them to the login page
+  res.redirect("/");
 }
