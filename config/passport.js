a6d88edc2fbb391e92d92caba65c453a2dc06e32
kcenters <kristincenters@gmail.com>
2020-05-18 22:58:12 -0500

    updated login code

--- config/passport.js
+++ config/passport.js
@@ -15,43 +15,68 @@ module.exports = function (passport, user) {
             var generateHash = function (password) {
                 return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
             };
-
-
             // When a user tries to sign in this code runs
             User.findOne({
                 where: {
                     email: email
                 }
             }).then(function (dbUser) {
-
                 if (dbUser) {
                     return done(null, false, { message: "That email is already taken" });
                 }
-
                 // If there's no user with the given email
                 else {
-
                     var userPassword = generateHash(password);
-
                     var data = {
                         email: email,
                         password: userPassword
                     }
-
                     User.create(data).then(function (newUser, created) {
                         if (!newUser) {
                             return done(null, false);
                         }
-
                         if (newUser) {
                             return done(null, newUser);
                         }
                     })
-
                 }
             });
         }
     ));
+    // Login registered user
+    passport.use('local-login', new LocalStrategy(
+        // Our user will sign in using an email, rather than a "username"
+        {
+            usernameField: "email",
+            passwordField: "password",
+            passReqToCallback: true // allows us to pass back the entire request to the callback
+        }, function (req, email, password, done) {
+            var User = user;
+            var isValidPassword = function (email, password) {
+                return bcrypt.compareSync(password, email);
+            }
+            console.log(email);
+            console.log(password);
+            User.findOne({
+                where: {
+                    email: email
+                }
+            }).then(function (dbUser) {
+                if (!dbUser) {
+                    return done(null, false, { message: 'Email does not exist' });
+                }
+
+                if (!isValidPassword(dbUser.password, password)) {
+                    return done(null, false, { message: 'Incorrect password.' });
+                }
+
+                var userinfo = dbUser.get();
+                return done(null, userinfo);
+            }).catch(function (err) {
+                console.log("Error:", err);
+                return done(null, false, { message: 'Something went wrong with your Login' });
+            });
+        }));
 
     // In order to help keep authentication state across HTTP requests,
     // Sequelize needs to serialize and deserialize the user
@@ -59,14 +84,12 @@ module.exports = function (passport, user) {
     passport.serializeUser(function (user, cb) {
         cb(null, user);
     });
-
     passport.deserializeUser(function (user, cb) {
 
         User.findOne({ where: { id: user.id } }).then(function (user) {
             if (user) {
                 cb(null, user.get());
             } else {
-
                 cb(user.errors, null);
             }
         });
